4875733c0fe73ec49c577cbc91555e0e23f9f73e
Divyarajsinh Dodia <divyarajsinhdev@gmail.com>
2024-06-28 22:12:42 +0530

    minor changes

--- Common_Tests/Properties/AssemblyInfo.cs
+++ Common_Tests/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿//// You can specify all the values or you can default the Build and Revision Numbers
 //// by using the '*' as shown below:
 //// [assembly: AssemblyVersion("1.0.*")]
-//using NUnit.Framework;
+using NUnit.Framework;
 
 //[assembly: Parallelizable(ParallelScope.Fixtures)]
 //// Change LevelOfParallelism to 4 when doing a checkin, Pipelines needs this to be set to 4
-//[assembly: LevelOfParallelism(10)] // Comment out while using chrome or use 8 - 10, firefox  is unstable above 5 parallel execution at a time
+[assembly: LevelOfParallelism(3)] // Comment out while using chrome or use 8 - 10, firefox  is unstable above 5 parallel execution at a time
